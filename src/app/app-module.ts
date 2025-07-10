6ddde995a76de9bbbd212c03e380a04d67654a7e
Richard K Hughes <grnganon@gmail.com>
2025-07-10 11:03:12 -0400

    Created sidebar and some styling

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { MainContainer } from './main-container/main-container';
+import { Sidebar } from './layout/sidebar/sidebar';
 
 @NgModule({
   declarations: [
     App,
-    MainContainer
+    MainContainer,
+    Sidebar
   ],
   imports: [
     BrowserModule,
